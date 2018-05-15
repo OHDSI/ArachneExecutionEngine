5583899c1dbdd243dc1bc91d4ad1229a76a1855b
Pavel Grafkin <pavgra@gmail.com>
2018-05-14 21:40:11 -0400

    ARACHNE-2359. Rollback drivers' loading changes

--- src/main/java/com/odysseusinc/arachne/executionengine/config/DriverManagerConfig.java
+++ src/main/java/com/odysseusinc/arachne/executionengine/config/DriverManagerConfig.java
@@ -2,9 +2,6 @@ package com.odysseusinc.arachne.executionengine.config;
 
 import java.sql.Driver;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
 import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,38 +13,12 @@ public class DriverManagerConfig {
     private static final Logger log = LoggerFactory.getLogger(DriverManagerConfig.class);
 
     @PostConstruct
-    public void driverManagerRedshiftWorkaround() {
-
-        List<String> supportedDrivers = Arrays.asList(
-                "org.postgresql.Driver",
-                "net.sourceforge.jtds.jdbc.Driver",
-                "com.amazon.redshift.jdbc.Driver",
-                "com.mysql.jdbc.Driver",
-                "com.mysql.fabric.jdbc.FabricMySQLDriver",
-                "com.microsoft.sqlserver.jdbc.SQLServerDriver",
-                "oracle.jdbc.OracleDriver",
-                "com.cloudera.impala.jdbc41.Driver"
-        );
+    public void logLoadedDrivers() {
 
         java.util.Enumeration<Driver> drivers =  DriverManager.getDrivers();
         while (drivers.hasMoreElements()) {
             Driver d = drivers.nextElement();
-            try {
-                DriverManager.deregisterDriver(d);
-            } catch (SQLException e) {
-                throw new RuntimeException("Could not deregister redshift driver");
-            }
+            log.info("Loaded JDBC driver: " + d.getClass().getName() + ". Version: " + d.getMajorVersion() + "." + d.getMinorVersion());
         }
-
-        supportedDrivers.forEach(driverName -> {
-            try {
-                Class<?> driverClass = Class.forName(driverName);
-                Driver driver = (java.sql.Driver)driverClass.newInstance();
-                DriverManager.registerDriver(driver);
-                log.info("Loaded JDBC driver: " + driverName);
-            } catch (Exception e) {
-                log.info("Failed to load JDBC driver: " + driverName);
-            }
-        });
     }
 }
