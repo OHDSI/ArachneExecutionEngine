8f85cfe94348f970678a79e746de5dc1cc149920
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-06-13 16:57:36 +0300

    fix PLP version

--- src/main/dist/libs.r
+++ src/main/dist/libs.r
@@ -8,26 +8,26 @@ if ("CohortMethod" %in% installed.packages() == FALSE){
   install_github("OHDSI/CohortMethod", ref="v2.4.4")
 }
 if ("DatabaseConnector" %in% installed.packages() == FALSE){
-  install_github("OHDSI/DatabaseConnector", ref="#2ae287e") # v2.0.2
+  install_github("OHDSI/DatabaseConnector", ref="33b2c0b") # v2.1.2
 }
 if ("EmpiricalCalibration" %in% installed.packages() == FALSE){
-  install_github("OHDSI/EmpiricalCalibration", ref="#320f670") # v1.3.5
+  install_github("OHDSI/EmpiricalCalibration", ref="320f670") # v1.3.5
 }
 if ("FeatureExtraction" %in% installed.packages() == FALSE){
   install_github("OHDSI/FeatureExtraction") # ref="v2.1.1"
 }
 if ("OhdsiRTools" %in% installed.packages() == FALSE){
-  install_github("OHDSI/OhdsiRTools", ref="#b938cf1") # v1.3.3
+  install_github("OHDSI/OhdsiRTools", ref="b938cf1") # v1.3.3
 }
 if ("SqlRender" %in% installed.packages() == FALSE){
-  install_github("OHDSI/SqlRender", ref="#558bbe0") # 1.4.7
+  install_github("OHDSI/SqlRender", ref="558bbe0") # 1.4.7
 }
 if ("Cyclops" %in% installed.packages() == FALSE){
-  install_github("OHDSI/Cyclops", ref="#08e76d6") # v1.3.1
+  install_github("OHDSI/Cyclops", ref="08e76d6") # v1.3.1
 }
 if ("PatientLevelPrediction" %in% installed.packages() == FALSE){
-#  install_github("OHDSI/PatientLevelPrediction", ref="#ef9eed3") #v1.2.2
-  install_github("odysseusinc/PatientLevelPrediction", ref="cast_fixes") #v1.2.2
+  install_github("OHDSI/PatientLevelPrediction", ref="474ed88") #v2.0.4+
+#  install_github("odysseusinc/PatientLevelPrediction")
 }
 
 if ("assertthat" %in% installed.packages() == FALSE){
