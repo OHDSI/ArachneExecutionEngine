7d948ae48e1237f4a87f7c9cba4711fc33f7dc9d
PGrafkin <pavgra@gmail.com>
2018-07-24 22:04:11 -0400

    ARACHNE-2539. Fixed execution of SQL containing multiple queries per file in Oracle

--- src/main/java/com/odysseusinc/arachne/executionengine/service/impl/SQLServiceImpl.java
+++ src/main/java/com/odysseusinc/arachne/executionengine/service/impl/SQLServiceImpl.java
@@ -22,6 +22,7 @@
 
 package com.odysseusinc.arachne.executionengine.service.impl;
 
+import com.odysseusinc.arachne.commons.types.DBMSType;
 import com.odysseusinc.arachne.execution_engine_common.api.v1.dto.AnalysisRequestDTO;
 import com.odysseusinc.arachne.execution_engine_common.api.v1.dto.AnalysisResultStatusDTO;
 import com.odysseusinc.arachne.execution_engine_common.api.v1.dto.DataSourceUnsecuredDTO;
@@ -50,8 +51,11 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.ohdsi.sql.SqlSplit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -94,52 +98,20 @@ public class SQLServiceImpl implements SQLService {
 
                     List<File> files = AnalisysUtils.getDirectoryItemsFiltered(file, SQL_MATCHER);
                     for (File sqlFile : files) {
-                        Path resultFile = null;
                         final String sqlFileName = sqlFile.getName();
-                        try (OutputStream outputStream = new ByteArrayOutputStream()) {
-                            Files.copy(sqlFile.toPath(), outputStream);
-                            try (Statement statement = conn.createStatement();) {
-                                boolean hasMoreResultSets = statement.execute(outputStream.toString());
-                                int resultIdx = 0;
-                                while (hasMoreResultSets || statement.getUpdateCount() != -1) {
-                                    if (hasMoreResultSets) {
-                                        try (ResultSet resultSet = statement.getResultSet()) {
-                                            if (resultSet != null) {
-                                                resultFile = Paths.get(sqlFile.getAbsolutePath() + ".result_" + resultIdx + ".csv");
-                                                try (PrintWriter out = new PrintWriter(new BufferedWriter(
-                                                        new FileWriter(resultFile.toFile(), true))
-                                                )) {
-                                                    ResultSetMetaData metaData = resultSet.getMetaData();
-                                                    int columnCount = metaData.getColumnCount();
-                                                    for (int column = 1; column <= columnCount; column++) {
-                                                        String columnLabel = metaData.getColumnLabel(column);
-                                                        out.append(columnLabel);
-                                                        if (column < columnCount) {
-                                                            out.append(csvSeparator);
-                                                        }
-                                                    }
-                                                    out.append("\r\n");
-                                                    while (resultSet.next()) {
-                                                        for (int ii = 1; ii <= columnCount; ii++) {
-                                                            Object object = resultSet.getObject(ii);
-                                                            out.print(object);
-                                                            if (ii < columnCount) {
-                                                                out.print(csvSeparator);
-                                                            }
-                                                        }
-                                                        out.print("\r\n");
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                    hasMoreResultSets = statement.getMoreResults();
-                                    resultIdx++;
-                                }
+                        try {
+                            SqlExecutor sqlExecutor;
+
+                            if (analysis.getDataSource().getType().equals(DBMSType.ORACLE)) {
+                                sqlExecutor = new OracleSqlExecutor();
+                            } else {
+                                sqlExecutor = new DefaultSqlExecutor();
                             }
+                            List<Path> resultFileList = sqlExecutor.runSql(conn, sqlFile);
+                            //
                             stdout.append(sqlFileName).append("\r\n\r\n").append("has been executed correctly").append("\r\n");
-                            if (resultFile != null) {
-                                stdout.append("has result file: ").append(resultFile.getFileName().toString());
+                            if (resultFileList.size() > 0) {
+                                stdout.append("has result file: ").append(resultFileList.stream().map(rf -> rf.getFileName().toString()).collect(Collectors.joining(", ")));
                             } else {
                                 stdout.append("does not have a result file");
                             }
@@ -180,4 +152,89 @@ public class SQLServiceImpl implements SQLService {
         });
     }
 
+    public abstract class SqlExecutor {
+        public abstract List<Path> runSql(Connection conn, File sqlFile) throws SQLException, IOException;
+
+        Path processResultSet(Statement statement, String resultFileName) throws IOException, SQLException {
+            Path resultFile = null;
+            try (ResultSet resultSet = statement.getResultSet()) {
+                if (resultSet != null) {
+                    resultFile = Paths.get(resultFileName);
+                    try (PrintWriter out = new PrintWriter(new BufferedWriter(
+                            new FileWriter(resultFile.toFile(), true))
+                    )) {
+                        ResultSetMetaData metaData = resultSet.getMetaData();
+                        int columnCount = metaData.getColumnCount();
+                        for (int column = 1; column <= columnCount; column++) {
+                            String columnLabel = metaData.getColumnLabel(column);
+                            out.append(columnLabel);
+                            if (column < columnCount) {
+                                out.append(csvSeparator);
+                            }
+                        }
+                        out.append("\r\n");
+                        while (resultSet.next()) {
+                            for (int ii = 1; ii <= columnCount; ii++) {
+                                Object object = resultSet.getObject(ii);
+                                out.print(object);
+                                if (ii < columnCount) {
+                                    out.print(csvSeparator);
+                                }
+                            }
+                            out.print("\r\n");
+                        }
+                    }
+                }
+            }
+            return resultFile;
+        }
+    }
+
+    public class DefaultSqlExecutor extends SqlExecutor {
+
+        public List<Path> runSql(Connection conn, File sqlFile) throws SQLException, IOException {
+
+            List<Path> resultFileList = new ArrayList<>();
+            try (OutputStream outputStream = new ByteArrayOutputStream()) {
+                Files.copy(sqlFile.toPath(), outputStream);
+                try (Statement statement = conn.createStatement();) {
+                    boolean hasMoreResultSets = statement.execute(outputStream.toString());
+                    int resultIdx = 0;
+                    while (hasMoreResultSets || statement.getUpdateCount() != -1) {
+                        if (hasMoreResultSets) {
+                            Path resultFile = processResultSet(statement, sqlFile.getAbsolutePath() + ".result_" + resultIdx + ".csv");
+                            if (resultFile != null) {
+                                resultFileList.add(resultFile);
+                            }
+                        }
+                        hasMoreResultSets = statement.getMoreResults();
+                        resultIdx++;
+                    }
+                }
+            }
+            return resultFileList;
+        }
+    }
+
+    public class OracleSqlExecutor extends SqlExecutor {
+
+        public List<Path> runSql(Connection conn, File sqlFile) throws SQLException, IOException {
+
+            List<Path> resultFileList = new ArrayList<>();
+            try (OutputStream outputStream = new ByteArrayOutputStream()) {
+                Files.copy(sqlFile.toPath(), outputStream);
+                try (Statement statement = conn.createStatement()) {
+                    String[] sqlParts = SqlSplit.splitSql(outputStream.toString());
+                    for (int i = 0; i < sqlParts.length; i++) {
+                        statement.execute(sqlParts[i]);
+                        Path resultFile = processResultSet(statement, sqlFile.getAbsolutePath() + ".result_" + i + ".csv");
+                        if (resultFile != null) {
+                            resultFileList.add(resultFile);
+                        }
+                    }
+                }
+            }
+            return resultFileList;
+        }
+    }
 }
