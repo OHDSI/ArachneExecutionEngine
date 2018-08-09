6a3d4a8a5748699cdc01b917a3b28aad680ff498
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2018-08-09 13:08:17 +0300

    ATLP-446. removed extra lines

--- src/main/docker/krb-with-R.sh
+++ src/main/docker/krb-with-R.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-RUN_KINIT="$1"
+RUN_KINIT=$1
 ANALYSIS_FILE=$2
 
-kinit "$RUN_KINIT"
-#kinit "$RUN_KINIT"
-
-#kinit -k -t /etc/krb.keytab ohdsi@ODYSSEUSINC.COM
-#kinit "-k -t /etc/krb.keytab ohdsi@ODYSSEUSINC.COM" >> /myoutput2.txt
-
-echo $RUN_KINIT >> /myoutput2.txt
-cat /etc/krb5.conf >> /myoutput2.txt
-cat /etc/krb.keytab >> /myoutput2.txt
-echo " KLIST OUTPUT :" >> /myoutput2.txt
-klist >> /myoutput2.txt
+kinit $RUN_KINIT
 Rscript /$ANALYSIS_FILE
