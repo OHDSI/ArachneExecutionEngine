13e7b4363bff734ae03f2877fb94b5d72751b84c
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2018-08-22 15:09:01 +0300

    ATLP-446. Fixed cp command in jail.sh

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -3,4 +3,4 @@
 if [ -f "/libs-r/$R_INSTALL_SCRIPT" ]; then
  Rscript /libs-r/$R_INSTALL_SCRIPT
 fi
-java -Djava.security.egd=file:/dev/./urandom -Dsun.security.krb5.debug=true -jar /execution-engine.jar
+java -Djava.security.egd=file:/dev/./urandom -jar /execution-engine.jar
