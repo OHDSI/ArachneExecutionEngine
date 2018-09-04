fdc902146662ec31c7cb8a171edee8cf8b6dd850
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2018-09-04 18:26:27 +0300

    ARACHNE-2555 Connection pooling for DN health check

---
+++ src/main/java/com/odysseusinc/arachne/executionengine/service/impl/HikariConnectionPoolService.java
@@ -0,0 +1,63 @@
+package com.odysseusinc.arachne.executionengine.service.impl;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.odysseusinc.arachne.execution_engine_common.api.v1.dto.DataSourceUnsecuredDTO;
+import com.odysseusinc.arachne.executionengine.service.ConnectionPoolService;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import javax.sql.DataSource;
+import org.ohdsi.sql.SqlTranslate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HikariConnectionPoolService implements ConnectionPoolService {
+
+    private static final String TEST_SQL = "select 1;";
+
+    private Cache<String, DataSource> dataSourceCache;
+
+    @Value("${connectionpool.capacity.min}")
+    private int minPoolSize;
+
+    @Value("${connectionpool.capacity.max}")
+    private int maxPoolSize;
+
+    @Value("${connectionpool.ttl.minutes}")
+    private int ttl;
+
+    public HikariConnectionPoolService() {
+
+        dataSourceCache = CacheBuilder.newBuilder()
+                .expireAfterAccess(ttl, TimeUnit.MINUTES)
+                .build();
+    }
+
+    @Override
+    public DataSource getDataSource(DataSourceUnsecuredDTO dataSourceDTO) {
+
+        try {
+            return dataSourceCache.get(dataSourceDTO.getConnectionString(), () -> buildDataSource(dataSourceDTO));
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private DataSource buildDataSource(DataSourceUnsecuredDTO dataSourceDTO) {
+
+        HikariConfig config = new HikariConfig();
+        config.setAutoCommit(true);
+        config.setJdbcUrl(dataSourceDTO.getConnectionString());
+        config.setUsername(dataSourceDTO.getUsername());
+        config.setPassword(dataSourceDTO.getPassword());
+        config.setMaximumPoolSize(maxPoolSize);
+        config.setMinimumIdle(minPoolSize);
+        String testSql = SqlTranslate.translateSql(TEST_SQL, dataSourceDTO.getType().getOhdsiDB()).replaceAll(";$", "");
+        config.setConnectionTestQuery(testSql);
+
+        return new HikariDataSource(config);
+    }
+}
