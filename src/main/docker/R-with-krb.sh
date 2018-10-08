38a146d54a058ff66af1e4caa9510f32198fbf0f
Anastasiia Klochkova <anastasiia.klochkova@odysseusinc.com>
2018-10-08 16:59:21 +0300

    ATLP-524. Renamed parameter

--- src/main/docker/R-with-krb.sh
+++ src/main/docker/R-with-krb.sh
@@ -2,14 +2,14 @@
 
 KINIT_PARAMS=$1
 ANALYSIS_FILE=$2
-DBMS_PASSWORD=$3
+KRB_PASSWORD=$3
 
 if [ -n "$KINIT_PARAMS" ]
 then
   if [[ "$KINIT_PARAMS" == *"keytab"* ]]; then
     kinit $KINIT_PARAMS
   else
-    echo $DBMS_PASSWORD | $KINIT_PARAMS
+    echo $KRB_PASSWORD | $KINIT_PARAMS
   fi
 fi
 
