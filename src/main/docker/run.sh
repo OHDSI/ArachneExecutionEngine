e6b13bec75fe9ba1fc900db8af970cca65c0fba7
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-04-02 20:04:08 +0300

    ArachneExecutionEngine-182 Add cron service to be installed and running in EE docker container

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,6 +1,39 @@
 #!/usr/bin/env bash
 
+service rsyslog start
+service logrotate start
+service cron start
+
 if [ -f "/libs-r/$R_INSTALL_SCRIPT" ]; then
  Rscript /libs-r/$R_INSTALL_SCRIPT
 fi
+
+#
+# Kerberos authentication scheduling
+#
+if [[ -n "${KRB5_USER}" ]]; then
+    echo "Kerberos init"
+    sed -i 's/\(default_realm = \).*/\1'"$KRB5_REALM"'/' /etc/krb5.conf
+    sed -i '/\[realms\]/a '"$KRB5_REALM"' = {\n\t\tkdc = '"$KRB5_KDC"'\n\t\tadmin_server = '"$KRB5_ADMIN_SERVER"'\n\t}' /etc/krb5.conf
+    sed -i '/PATH/a MAILTO=""' /etc/crontab
+
+    KRB_REFRESH="/bin/krb-refresh"
+    CRON_EXPR="*/5 * * * *  root  $KRB_REFRESH 2>&1 >/dev/null | /usr/bin/logger -t kerberos"
+
+    if [[ -n "${KRB5_PASSWORD}" ]]; then
+        echo "${KRB5_PASSWORD}" | kinit "${KRB5_USER}"
+        echo "#!/usr/bin/env bash
+        echo ${KRB5_PASSWORD} | kinit ${KRB5_USER}" > $KRB_REFRESH
+    elif [[ -n "${KRB5_KEYTAB}" ]]; then
+        kinit -k -t "${KRB5_KEYTAB}" "${KRB5_USER}"
+        echo "#!/usr/bin/env bash
+        kinit -k -t ${KRB5_KEYTAB} ${KRB5_USER}" > $KRB_REFRESH
+    else
+        echo "Either KRB5_PASSWORD or KRB5_KEYTAB is required"
+        exit 1
+    fi
+    chmod a+x $KRB_REFRESH
+    grep -q -F "$CRON_EXPR" /etc/crontab || echo "$CRON_EXPR" >> /etc/crontab
+fi
+
 java -Djava.security.egd=file:/dev/./urandom -jar /execution-engine.jar
