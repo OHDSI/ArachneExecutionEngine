ca65df0cc2a176bacaf7ce13765067d15b4c7add
PGrafkin <pavgra@gmail.com>
2019-06-27 23:32:00 -0400

    Do not show env variables in ps

--- src/main/resources/jail.sh
+++ src/main/resources/jail.sh
@@ -29,14 +29,23 @@ sudo cp /etc/R-with-krb.sh $JAIL/etc/R-with-krb.sh
 sudo cp -R /impala/. $JAIL/impala/
 sudo chmod +x $JAIL/etc/R-with-krb.sh
 
+echo -e "#!/usr/bin/env bash \n \
+    export DATA_SOURCE_NAME=\"$DATA_SOURCE_NAME\" \n \
+    export DBMS_USERNAME=\"$DBMS_USERNAME\" \n \
+    export DBMS_PASSWORD=\"$DBMS_PASSWORD\" \n \
+    export DBMS_TYPE=\"$DBMS_TYPE\" \n \
+    export CONNECTION_STRING=\"$CONNECTION_STRING\" \n \
+    export DBMS_SCHEMA=\"$DBMS_SCHEMA\" \n \
+    export TARGET_SCHEMA=\"$TARGET_SCHEMA\" \n \
+    export RESULT_SCHEMA=\"$RESULT_SCHEMA\" \n \
+    export COHORT_TARGET_TABLE=\"$COHORT_TARGET_TABLE\" \n \
+    export ANALYSIS_ID=\"$ANALYSIS_ID\" \n \
+    export PATH=\"$PATH\" \n \
+    export HOME=\"$HOME\" \n \
+    export JDBC_DRIVER_PATH=\"$JDBC_DRIVER_PATH\" \n \
+    $(tail --lines=+2 $JAIL/etc/R-with-krb.sh)" > $JAIL/etc/R-with-krb.sh
+
 sudo unshare --fork --pid -- chroot $JAIL /bin/bash -c " \
     mount -t proc proc /proc && \
-    /usr/bin/env -i \
-     DATA_SOURCE_NAME=\"$DATA_SOURCE_NAME\" DBMS_USERNAME=$DBMS_USERNAME \
-     DBMS_PASSWORD=$DBMS_PASSWORD DBMS_TYPE=$DBMS_TYPE \
-     CONNECTION_STRING=$CONNECTION_STRING DBMS_SCHEMA=$DBMS_SCHEMA \
-     TARGET_SCHEMA=$TARGET_SCHEMA RESULT_SCHEMA=$RESULT_SCHEMA \
-     COHORT_TARGET_TABLE=$COHORT_TARGET_TABLE ANALYSIS_ID=$ANALYSIS_ID PATH=$PATH \
-     HOME=$HOME JDBC_DRIVER_PATH=$JDBC_DRIVER_PATH \
-     /etc/R-with-krb.sh \"$KINIT_PARAMS\" \"$ANALYSIS_FILE\" \"$KRB_PASSWORD\" \
+    ./etc/R-with-krb.sh \"$KINIT_PARAMS\" \"$ANALYSIS_FILE\" \"$KRB_PASSWORD\" \
 "
