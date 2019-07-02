4b89cd150c72f9f741abb69008231d829e11dc81
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-07-02 18:18:03 +0300

    Fixes NPE during non-Kerberos auth (#231)

--- src/main/java/com/odysseusinc/arachne/executionengine/service/impl/AnalysisServiceImpl.java
+++ src/main/java/com/odysseusinc/arachne/executionengine/service/impl/AnalysisServiceImpl.java
@@ -125,10 +125,13 @@ public class AnalysisServiceImpl implements AnalysisService, InitializingBean {
             List<Optional> results = authResolvers.stream().filter(r -> r.supports(dataSourceData))
                     .map(r -> r.resolveAuth(dataSourceData, keystoreDir))
                     .collect(Collectors.toList());
-            KrbConfig krbConfig = (KrbConfig) results.stream().filter(r -> r.isPresent() && r.get() instanceof KrbConfig)
-                    .findFirst()
-                    .orElse(Optional.empty())
-                    .get();
+						KrbConfig krbConfig = new KrbConfig();
+						for(Optional val : results){
+							if (val.isPresent() && val.get() instanceof KrbConfig) {
+								krbConfig = (KrbConfig) val.get();
+								break;
+							}
+						}
 
             String executableFileName = analysis.getExecutableFileName();
             String fileExtension = Files.getFileExtension(executableFileName).toLowerCase();
