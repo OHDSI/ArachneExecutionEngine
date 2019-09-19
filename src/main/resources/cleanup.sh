313a77ef9db076b77ca5a87ca4545253970aaa60
PGrafkin <pavgra@gmail.com>
2019-09-18 21:22:30 -0400

    Revert folder deletion on cleanup

--- src/main/resources/cleanup.sh
+++ src/main/resources/cleanup.sh
@@ -3,3 +3,9 @@
 WORKDIR=$1
 sudo umount $WORKDIR/proc
 sudo umount "$WORKDIR"_merged
+dirs=( bin boot dev etc home impala lib lib64 libs.r media mnt opt proc root run sbin srv sys tmp usr var .Rhistory )
+
+for d in "${dirs[@]}"
+do
+    sudo rm -fr $jail/$d
+done
