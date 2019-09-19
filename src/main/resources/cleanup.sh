3067777939132fd8665f9fa8fedd512486b65c85
PGrafkin <pavgra@gmail.com>
2019-09-18 21:47:31 -0400

    Remove created folders

--- src/main/resources/cleanup.sh
+++ src/main/resources/cleanup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 WORKDIR=$1
+
 sudo umount $WORKDIR/proc
 sudo umount "$WORKDIR"_merged
+sudo rm -rf "$WORKDIR"_overlay_work
+sudo rm -rf "$WORKDIR"_overlay_merged
+
 dirs=( bin boot dev etc home impala lib lib64 libs.r media mnt opt proc root run sbin srv sys tmp usr var .Rhistory )
 
 for d in "${dirs[@]}"
