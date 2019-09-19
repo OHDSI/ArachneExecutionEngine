0bbbe53a983b0ffc5140d10d13b2e0c5fa7db0b1
PGrafkin <pavgra@gmail.com>
2019-09-18 22:07:37 -0400

    Rollback

--- src/main/resources/cleanup.sh
+++ src/main/resources/cleanup.sh
@@ -4,8 +4,8 @@ WORKDIR=$1
 
 sudo umount $WORKDIR/proc
 sudo umount "$WORKDIR"_merged
-sudo rm -rf "$WORKDIR"_overlay_work
-sudo rm -rf "$WORKDIR"_overlay_merged
+# sudo rm -rf "$WORKDIR"_overlay_work
+# sudo rm -rf "$WORKDIR"_overlay_merged
 
 dirs=( bin boot dev etc home impala lib lib64 libs.r media mnt opt proc root run sbin srv sys tmp usr var .Rhistory )
 
