60105a8d4fab83898e0118a8983bbe29c9628bc6
YaroslavTir <yaroslav.molodkov@odysseusinc.com>
2019-12-18 12:06:10 +0100

    check knit info

--- src/main/docker/R-with-krb.sh
+++ src/main/docker/R-with-krb.sh
@@ -4,13 +4,16 @@ KINIT_PARAMS=$1
 ANALYSIS_FILE=$2
 KRB_PASSWORD=$3
 
+
 if [ -n "$KINIT_PARAMS" ]
 then
   if [[ "$KINIT_PARAMS" == *"keytab"* ]]; then
+    echo "kinit $KINIT_PARAMS"
     kinit $KINIT_PARAMS
   else
     echo $KRB_PASSWORD | $KINIT_PARAMS
   fi
 fi
 
+echo "before rscript"
 Rscript /$ANALYSIS_FILE
