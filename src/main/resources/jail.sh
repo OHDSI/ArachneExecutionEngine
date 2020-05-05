6a7bc4108215bd60f2b219957ac44413fe484f9a
alexandr cumarav <https://localhost:8080/study-manager/studies/6>
2020-05-05 16:32:48 +0200

    execution engine 1.16.1 RC

--- src/main/resources/jail.sh
+++ src/main/resources/jail.sh
@@ -30,20 +30,20 @@ sudo cp /etc/R-with-krb.sh $JAIL/etc/R-with-krb.sh
 sudo cp -R /impala/. $JAIL/impala/
 sudo chmod +x $JAIL/etc/R-with-krb.sh
 
-sudo echo -e "#!/usr/bin/env bash \n \
-    export DATA_SOURCE_NAME=\"$DATA_SOURCE_NAME\" \n \
-    export DBMS_USERNAME=\"$DBMS_USERNAME\" \n \
-    export DBMS_PASSWORD=\"$DBMS_PASSWORD\" \n \
-    export DBMS_TYPE=\"$DBMS_TYPE\" \n \
-    export CONNECTION_STRING=\"$CONNECTION_STRING\" \n \
-    export DBMS_SCHEMA=\"$DBMS_SCHEMA\" \n \
-    export TARGET_SCHEMA=\"$TARGET_SCHEMA\" \n \
-    export RESULT_SCHEMA=\"$RESULT_SCHEMA\" \n \
-    export COHORT_TARGET_TABLE=\"$COHORT_TARGET_TABLE\" \n \
-    export ANALYSIS_ID=\"$ANALYSIS_ID\" \n \
-    export PATH=\"$PATH\" \n \
-    export HOME=\"$HOME\" \n \
-    export JDBC_DRIVER_PATH=\"$JDBC_DRIVER_PATH\" \n \
+sudo printf "#!/usr/bin/env bash\n \
+	export DATA_SOURCE_NAME='$DATA_SOURCE_NAME'\n \
+	export DBMS_USERNAME='$DBMS_USERNAME'\n	\
+	export DBMS_PASSWORD='$DBMS_PASSWORD'\n \
+	export DBMS_TYPE='$DBMS_TYPE'\n \
+	export CONNECTION_STRING='$CONNECTION_STRING'\n \
+	export DBMS_SCHEMA='$DBMS_SCHEMA'\n \
+	export TARGET_SCHEMA='$TARGET_SCHEMA'\n \
+	export RESULT_SCHEMA='$RESULT_SCHEMA'\n \
+	export COHORT_TARGET_TABLE='$COHORT_TARGET_TABLE'\n \
+	export ANALYSIS_ID='$ANALYSIS_ID'\n \
+	export PATH='$PATH'\n \
+	export HOME='$HOME'\n \
+	export JDBC_DRIVER_PATH='$JDBC_DRIVER_PATH' \n \
     $(tail --lines=+2 $JAIL/etc/R-with-krb.sh)" | sudo tee $JAIL/etc/R-with-krb.sh > /dev/null
 
 sudo unshare --fork --pid -- chroot $JAIL /bin/bash -c " \
