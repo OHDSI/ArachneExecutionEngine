2c203fdb51fc3f3c0e83dbb9f04e423997b77fc4
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2020-05-20 17:42:33 +0300

    source short name was sanitized to prevent zipping problem in case when short name contains slash(https://github.com/OHDSI/WebAPI/issues/1542)

--- src/main/java/com/odysseusinc/arachne/executionengine/service/impl/RuntimeServiceImpl.java
+++ src/main/java/com/odysseusinc/arachne/executionengine/service/impl/RuntimeServiceImpl.java
@@ -23,6 +23,7 @@
 package com.odysseusinc.arachne.executionengine.service.impl;
 
 import com.odysseusinc.arachne.commons.types.DBMSType;
+import com.odysseusinc.arachne.commons.utils.CommonFilenameUtils;
 import com.odysseusinc.arachne.execution_engine_common.api.v1.dto.AnalysisRequestDTO;
 import com.odysseusinc.arachne.execution_engine_common.api.v1.dto.AnalysisResultStatusDTO;
 import com.odysseusinc.arachne.execution_engine_common.api.v1.dto.AnalysisSyncRequestDTO;
@@ -252,7 +253,7 @@ public class RuntimeServiceImpl implements RuntimeService {
     private Map<String, String> buildRuntimeEnvVariables(DataSourceUnsecuredDTO dataSource, Map<String, String> krbProps) {
 
         Map<String, String> environment = new HashMap<>(krbProps);
-        environment.put(RUNTIME_ENV_DATA_SOURCE_NAME, dataSource.getName());
+        environment.put(RUNTIME_ENV_DATA_SOURCE_NAME, CommonFilenameUtils.sanitizeFilename(dataSource.getName()));
         environment.put(RUNTIME_ENV_DBMS_USERNAME, dataSource.getUsername());
         environment.put(RUNTIME_ENV_DBMS_PASSWORD, dataSource.getPassword());
         environment.put(RUNTIME_ENV_DBMS_TYPE, dataSource.getType().getOhdsiDB());
