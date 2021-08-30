d4238d02f0d4b9f3e944a458549d9f8fe9fcab38
alex-odysseus <75131044+alex-odysseus@users.noreply.github.com>
2021-08-30 10:56:27 +0200

    Merging 1.19.0 'develop' to 'qa' (#331)
    * Change version to 1.19.0-SNAPSHOT
    
    * Raise hydra version (#326)
    
    * Added possibility to work with external postgresql, ms sql, redshift and oracle drivers
    
    * Remove custom miniconda installation as it's called from R
    
    * Return back miniconda setup
    
    * adding alias for python3
    
    * Fixes to conda packages
    
    * Fix for keras setup
    
    * PLP Test was added
    
    * PLP Test was added
    
    * Fix bash issue
    
    * Fix bash issue
    
    * Fix bash issue
    
    * Fix bash issue
    
    * Fixing PLP test
    
    * Fixing PLP test
    
    * Fixing PLP test
    
    * Fixing PLP test
    
    * restored spotify plugin
    
    * added new parameters to README
    
    * Added comments for variables
    
    Co-authored-by: Sergey Suvorov <sergey.suvorov@odysseusinc.com>
    Co-authored-by: konstjar <konstjar@users.noreply.github.com>
    Co-authored-by: Konstantin Yaroshovets <konstantin.yaroshovets@odysseusinc.com>
    
    * Fix drivers copy (#328)
    
    * redshift driver is now excluded from analysis (#329)
    
    * Updated reference to r-env:1.0.16 (#330)
    
    * 1.19.0-QA version
    
    Co-authored-by: Sergey Suvorov <sergey.suvorov@odysseusinc.com>
    Co-authored-by: Sergey Suvorov <sergey.suvorov@firstlinesoftware.com>
    Co-authored-by: konstjar <konstjar@users.noreply.github.com>
    Co-authored-by: Konstantin Yaroshovets <konstantin.yaroshovets@odysseusinc.com>

--- src/main/dist/jenkins.sample.sh
+++ src/main/dist/jenkins.sample.sh
@@ -4,5 +4,13 @@ mkdir -p $WORKSPACE/src/main/extras/netezza
 cp /var/local/drivers/netezza/*.jar $WORKSPACE/src/main/extras/netezza/
 mkdir -p $WORKSPACE/src/main/extras/bigquery
 cp /var/local/drivers/bigquery/*.jar $WORKSPACE/src/main/extras/bigquery/
+mkdir -p $WORKSPACE/src/main/extras/postgresql
+cp /var/local/drivers/postgresql/*.jar $WORKSPACE/src/main/extras/postgresql/
+mkdir -p $WORKSPACE/src/main/extras/redshift
+cp /var/local/drivers/redshift/*.jar $WORKSPACE/src/main/extras/redshift/
+mkdir -p $WORKSPACE/src/main/extras/mssql
+cp /var/local/drivers/mssql/*.jar $WORKSPACE/src/main/extras/mssql/
+mkdir -p $WORKSPACE/src/main/extras/oracle
+cp /var/local/drivers/oracle/*.jar $WORKSPACE/src/main/extras/oracle/
 cd $WORKSPACE/src/main/dist
 sudo /bin/bash run_build.sh -d xenial
