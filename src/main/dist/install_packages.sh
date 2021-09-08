ec17bfe33cc206eaf2b23c32fc7d90663334b7f5
alex-odysseus <75131044+alex-odysseus@users.noreply.github.com>
2021-09-08 14:13:54 +0200

    1.19.0 qa to master (#334)
    * Merging 1.19.0 'develop' to 'qa' (#331)
    
    * Change version to 1.19.0-SNAPSHOT
    
    * Raise hydra version (#326)
    
    * Added possibility to work with external postgresql, ms sql, redshift and oracle drivers
    
    * Remove custom miniconda installation as it's called from R
    
    * Return back miniconda setup
    
    * adding alias for python3
    
    * Fixes to conda packages
    
    * Fix for keras setup
    
    * PLP Test was added
    
    * PLP Test was added
    
    * Fix bash issue
    
    * Fix bash issue
    
    * Fix bash issue
    
    * Fix bash issue
    
    * Fixing PLP test
    
    * Fixing PLP test
    
    * Fixing PLP test
    
    * Fixing PLP test
    
    * restored spotify plugin
    
    * added new parameters to README
    
    * Added comments for variables
    
    Co-authored-by: Sergey Suvorov <sergey.suvorov@odysseusinc.com>
    Co-authored-by: konstjar <konstjar@users.noreply.github.com>
    Co-authored-by: Konstantin Yaroshovets <konstantin.yaroshovets@odysseusinc.com>
    
    * Fix drivers copy (#328)
    
    * redshift driver is now excluded from analysis (#329)
    
    * Updated reference to r-env:1.0.16 (#330)
    
    * 1.19.0-QA version
    
    Co-authored-by: Sergey Suvorov <sergey.suvorov@odysseusinc.com>
    Co-authored-by: Sergey Suvorov <sergey.suvorov@firstlinesoftware.com>
    Co-authored-by: konstjar <konstjar@users.noreply.github.com>
    Co-authored-by: Konstantin Yaroshovets <konstantin.yaroshovets@odysseusinc.com>
    
    * updated id and name
    
    * SqlRender 1.7.0 dependency (#333)
    
    * 1.19.0 release
    
    * fixed typo
    
    Co-authored-by: Sergey Suvorov <sergey.suvorov@odysseusinc.com>
    Co-authored-by: Sergey Suvorov <sergey.suvorov@firstlinesoftware.com>
    Co-authored-by: konstjar <konstjar@users.noreply.github.com>
    Co-authored-by: Konstantin Yaroshovets <konstantin.yaroshovets@odysseusinc.com>

--- src/main/dist/install_packages.sh
+++ src/main/dist/install_packages.sh
@@ -2,9 +2,11 @@
 
 # DIST - Ubuntu Dist
 # CRAN_URL - CRAN Mirror (eg https://cran.asia/)
+# JDBC_TEST - JDBC Connection string for PLP test
 
 DIST=$1
 CRAN_URL=$2
+JDBC_TEST=$3
 
 HOME=/root
 
@@ -38,7 +40,7 @@ update-alternatives --config java
 
 # Doesn't work for Bionic
 #sudo add-apt-repository -y ppa:deadsnakes/ppa
-apt update && apt install -yf libpq-dev build-essential gcc make libcurl4-openssl-dev libssl-dev curl libssh-dev libxml2-dev libdigest-hmac-perl libcairo2-dev wget unzip apt-transport-https python-dev krb5-user virtualenv libgeos-dev libprotobuf-dev protobuf-compiler
+apt update && apt install -yf libpq-dev libgit2-dev libssh2-1-dev build-essential gcc make libcurl4-openssl-dev libssl-dev curl libssh-dev libxml2-dev libdigest-hmac-perl libcairo2-dev wget unzip apt-transport-https python-dev krb5-user virtualenv libgeos-dev libprotobuf-dev protobuf-compiler
 
 wget http://cdn.azul.com/zcek/bin/ZuluJCEPolicies.zip \
         && echo "8021a28b8cac41b44f1421fd210a0a0822fcaf88d62d2e70a35b2ff628a8675a  ZuluJCEPolicies.zip" | sha256sum -c - \
@@ -89,8 +91,10 @@ export PATH=$PATH:/root/miniconda/bin
 conda create -y -n PLP python=3.8.3
 conda install -y -n PLP -c sebp scikit-survival=0.12.0
 conda install -y -n PLP -c pytorch pytorch torchvision
-
 rm -f /Miniconda3-4.5.12-Linux-x86_64.sh
+echo 'alias python=python3' >> /root/.bashrc
+alias python=python3
+conda update -y -n base conda
 
 R CMD javareconf
 Rscript /root/libs/libs_1.r
@@ -99,3 +103,31 @@ Rscript /root/libs/libs_3.r
 Rscript /root/libs/libs_4.r
 Rscript /root/libs/libs_5.r
 Rscript /root/libs/libs_6.r
+
+# Required by keras
+conda remove -y -n PLP PyYAML
+
+Rscript /root/libs/libs_7.r
+
+
+# Run PLP test
+mkdir /opt/drivers
+echo "DATABASECONNECTOR_JAR_FOLDER=/opt/drivers/" >> /root/.Renviron
+cat >> /root/libs/plp_test.r <<EOF
+library(DatabaseConnector)
+downloadJdbcDrivers("redshift")
+downloadJdbcDrivers("postgresql")
+downloadJdbcDrivers("oracle")
+downloadJdbcDrivers("sql server")
+connectionDetails <- createConnectionDetails(dbms = "postgresql", connectionString = "$JDBC_TEST", pathToDriver="/postgresql")
+PatientLevelPrediction::checkPlpInstallation(connectionDetails = connectionDetails, python = T)
+EOF
+if [ -z "${JDBC_TEST}" ]; then
+  echo "Skipping PLP test, no JDBC connection string"
+else
+  echo "Running PLP test"
+  # Update: test was disabled due to error
+  # https://github.com/OHDSI/PatientLevelPrediction/issues/229
+  # Rscript /root/libs/plp_test.r
+fi
+
--- src/main/resources/cleanup.sh
+++ src/main/resources/cleanup.sh
@@ -2,7 +2,7 @@
 
 jail=$1
 sudo umount $jail/proc
-dirs=( bin bigquery boot dev etc home hive impala lib lib32 libx32 lib64 libs.r media mnt netezza opt proc root run sbin srv sys tmp usr var .Rhistory )
+dirs=( bin bigquery boot dev etc home hive impala lib lib32 libx32 lib64 libs.r media mnt mssql netezza opt oracle postgresql proc redshift root run sbin srv sys tmp usr var .Rhistory )
 
 for d in "${dirs[@]}"
 do
