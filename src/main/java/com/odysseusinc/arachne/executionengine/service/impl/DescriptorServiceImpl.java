c31090505e74ef251cd9f0a55185ce64c6ec1661
sergey-suvorov <sergey.suvorov@odysseusinc.com>
2023-06-22 19:49:17 +0300

    version was changed to 1.21.1-alpha

--- src/main/java/com/odysseusinc/arachne/executionengine/service/impl/DescriptorServiceImpl.java
+++ src/main/java/com/odysseusinc/arachne/executionengine/service/impl/DescriptorServiceImpl.java
@@ -117,6 +117,9 @@ public class DescriptorServiceImpl implements DescriptorService {
             // if there're no required runtimes then each available descriptor will match
             // in case of empty list of required runtimes then we have to use default descriptor
             if (executionRuntimes.isEmpty()) {
+                if (rIsolatedRuntimeProps.isVerboseLog()) {
+                    LOGGER.info("Required runtimes are empty. Using default descriptor");
+                }
                 return Optional.empty();
             }
 
